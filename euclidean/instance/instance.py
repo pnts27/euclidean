fe82a8e752db56d991f5791e7156af6c87f6dd13
Pontus <pontus@pontus-pc>
2020-09-14 09:33:56 +0200

    Adds instance class

---
+++ euclidean/instance/instance.py
@@ -0,0 +1,21 @@
+from collections.abc import Iterable
+
+class Instance():
+    def __init__(self,
+                 clients: Iterable,
+                 facilities: Iterable,
+                 k: int):
+        self.__c = set(clients)
+        self.__f = set(facilities)
+        self.__k = k
+
+    def clients(self):
+        for c in self.__c:
+            yield c
+
+    def facilities(self):
+        for f in self.__f:
+            yield f
+
+    def getNumberOfClusterCenters(self):
+        return self.__k
