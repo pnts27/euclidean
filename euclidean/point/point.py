3c54e6136838f67586693335df074273638e243e
Pontus <pontus@pontus-pc>
2020-09-14 09:36:28 +0200

    Adds methods to obtain and iterate over coordinates of a point

--- euclidean/point/point.py
+++ euclidean/point/point.py
@@ -1,3 +1,10 @@
 class Point():
     def __init__(self, x, y, *args):
         self.__pos = (x,y) + tuple(args)
+
+    def getCoordinates(self):
+        return self.__pos
+
+    def coordiantes(self):
+        for coord in self.__pos:
+            yield coord
